4343c1c3bbb4d32344cd20b5c9a80b90e4c9a015
Rushs321 <fyggh566@tutamail.com>
2024-08-21 17:39:46 +0530

    Update redirect.js

--- src/redirect.js
+++ src/redirect.js
@@ -1,15 +1,19 @@
-function redirect(req, res) {
-  if (res.headersSent) {
+// ./src/redirect.js
+
+function redirect(request, reply) {
+  if (reply.sent) {
     return;
   }
 
-  res.setHeader('content-length', 0);
-  res.removeHeader('cache-control');
-  res.removeHeader('expires');
-  res.removeHeader('date');
-  res.removeHeader('etag');
-  res.setHeader('location', encodeURI(req.params.url));
-  res.status(302).end();
+  reply
+    .header('content-length', 0)
+    .removeHeader('cache-control')
+    .removeHeader('expires')
+    .removeHeader('date')
+    .removeHeader('etag')
+    .header('location', encodeURI(request.params.url))
+    .code(302)
+    .send();
 }
 
 module.exports = redirect;
