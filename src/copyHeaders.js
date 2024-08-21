260baed20470fc66c4a57d842c4b14d2fb8cbab2
Rushs321 <fyggh566@tutamail.com>
2024-08-21 17:22:01 +0530

    Update copyHeaders.js

--- src/copyHeaders.js
+++ src/copyHeaders.js
@@ -1,7 +1,7 @@
 function copyHeaders(source, target) {
   for (const [key, value] of Object.entries(source.headers)) {
     try {
-      target.setHeader(key, value);
+      target.header(key, value);
     } catch (e) {
       console.log(e.message);
     }
