4a725207dc6c1b68a46c95679c614f66ac6c65b1
Rushs321 <fyggh566@tutamail.com>
2024-08-22 16:10:35 +0530

    Update bypass.js

--- src/bypass.js
+++ src/bypass.js
@@ -1,5 +1,3 @@
-import { header, code, send } from 'fastify';
-
 export function bypass(request, reply, buffer) {
   reply.header('x-proxy-bypass', 1);
   reply.header('content-length', buffer.length);
