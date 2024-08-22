b809911fb7fa9771f981a07b15b64629a676dfc8
Rushs321 <fyggh566@tutamail.com>
2024-08-22 11:52:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 #!/usr/bin/env node
 'use strict';
 
-const fastify = require('fastify')({ 
+import fastify from 'fastify';
+import proxy from './src/proxy.js'; // Ensure to use .js or .mjs if needed
+
+const app = fastify({ 
   logger: true,
   disableRequestLogging: true,
   trustProxy: true // Enable trust proxy
 });
-const proxy = require('./src/proxy');
 
 const PORT = process.env.PORT || 8080;
 
 // Set up the route
-fastify.get('/', async (request, reply) => {
+app.get('/', async (request, reply) => {
   return proxy(request, reply);
 });
 
 // Start the server
 const start = async () => {
- try {
-    await fastify.listen({ host: '0.0.0.0', port: PORT });
+  try {
+    await app.listen({ host: '0.0.0.0', port: PORT });
     console.log(`Listening on ${PORT}`);
   } catch (err) {
-    fastify.log.error(err);
+    app.log.error(err);
     process.exit(1);
   }
 };
