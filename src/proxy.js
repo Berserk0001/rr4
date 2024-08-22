06c79acb9eab4c4061d3d4fb6f0d6e057906974f
Rushs321 <fyggh566@tutamail.com>
2024-08-22 12:31:43 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch';
 import _ from 'lodash'; // Import lodash
 const { pick } = _; // Access 'pick' from lodash
-import utils from './utils.js'; // Import default export from utils
-const { generateRandomIP, randomUserAgent } = utils; // Destructure the functions
+import nutils from './utils'; // Import default export from utils
+const { generateRandomIP, randomUserAgent } = nutils; // Destructure the functions
 import copyHdrs from './copyHeaders';
 import applyCompression from './compress';
 import performBypass from './bypass';
