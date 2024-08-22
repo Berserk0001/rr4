841a304d10917529d182e38d7c4508f72dd4532d
Rushs321 <fyggh566@tutamail.com>
2024-08-22 15:49:42 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,22 +1,20 @@
-import imgProc from 'sharp';
-import redirectFunc from './redirect';
+import sharp from 'sharp';
+import redirectFunc from './redirect.js';
 
-async function compressImg(request, reply, imgData) {
-    const imgFormat = request.params.webp ? 'webp' : 'jpeg';
-    try {
-        const outputBuffer = await imgProc(imgData)
-            .grayscale(request.params.grayscale)
-            .toFormat(imgFormat, { quality: request.params.quality, progressive: true, optimizeScans: true, chromaSubsampling: '4:4:4', })
-            .toBuffer({ resolveWithObject: true });
+export async function compressImg(request, reply, imgData) {
+  const imgFormat = request.params.webp ? 'webp' : 'jpeg';
+  try {
+    const outputBuffer = await sharp(imgData)
+      .grayscale(request.params.grayscale)
+      .toFormat(imgFormat, { quality: request.params.quality, progressive: true, optimizeScans: true, chromaSubsampling: '4:4:4' })
+      .toBuffer({ resolveWithObject: true });
 
-        reply.header('content-type', `image/${imgFormat}`);
-        reply.header('content-length', outputBuffer.info.size);
-        reply.header('x-original-size', request.params.originSize);
-        reply.header('x-bytes-saved', request.params.originSize - outputBuffer.info.size);
-        return reply.code(200).send(outputBuffer.data);
-    } catch (error) {
-        return redirectFunc(request, reply);
-    }
+    reply.header('content-type', `image/${imgFormat}`);
+    reply.header('content-length', outputBuffer.info.size);
+    reply.header('x-original-size', request.params.originSize);
+    reply.header('x-bytes-saved', request.params.originSize - outputBuffer.info.size);
+    return reply.code(200).send(outputBuffer.data);
+  } catch (error) {
+    return redirectFunc(request, reply);
+  }
 }
-
-export default compressImg;
