6d794d995d97ef88f13ccff1ec85b623506672ea
Rushs321 <fyggh566@tutamail.com>
2024-08-27 13:01:56 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,5 +1,5 @@
 import sharp from 'sharp';
-import { redirect } from './redirect.js';
+import { redirect } from './rrd.js';
 
 export async function compressImg(request, reply, imgData) {
     const { webp, grayscale, quality, originSize } = request.params;
