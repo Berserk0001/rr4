16015620ec17f5aa28eb494164f2ad6d946bf1d8
Rushs321 <fyggh566@tutamail.com>
2024-09-02 14:08:08 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -2,7 +2,7 @@
 const MIN_COMPRESS_LENGTH = 512; // Adjust the minimum compress length as desired
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
 
-function shouldCompress(req) {
+function shouldCompress(request) {
   const { originType, originSize, webp } = request.params;
 
   if (!originType.startsWith('image')) {
