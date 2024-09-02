cabac41169dcf6b88eeeb22957a38029f518f1a4
Rushs321 <fyggh566@tutamail.com>
2024-09-02 14:06:04 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -3,7 +3,7 @@ const MIN_COMPRESS_LENGTH = 512; // Adjust the minimum compress length as desire
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
 
 function shouldCompress(req) {
-  const { originType, originSize, webp } = req.params;
+  const { originType, originSize, webp } = request.params;
 
   if (!originType.startsWith('image')) {
     return false;
