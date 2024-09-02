1225e7c6ec7911e77464329e757c26663be442a5
Rushs321 <fyggh566@tutamail.com>
2024-09-02 14:17:48 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,6 +1,6 @@
 "use strict";
-const MIN_COMPRESS_LENGTH = 512; // Adjust the minimum compress length as desired
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 10;
+const MIN_COMPRESS_LENGTH = 512; // Minimum size in bytes to apply compression
+const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
 
 export function shouldCompress(request) {
   const { originType, originSize, webp } = request.params;
@@ -20,4 +20,3 @@ export function shouldCompress(request) {
 
   return true;
 }
-
